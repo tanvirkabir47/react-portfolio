2934b7bcbe3fa42a5b3b2900fe6b8300fc418ec5
tanvirkabir47 <tanvirkabir47@gmail.com>
2024-12-24 01:55:51 +0600

    update single blog page frontend

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,9 +1,54 @@
 import SingleBlogHero from "../components/HeroComponents/SingleBlogHero";
+import blogimg from "../assets/blog.jpg"
+import bannerImg from '../assets/profile-1.jpeg'
 
 const SingleBlog = () => {
     return (
         <>
             <SingleBlogHero/>
+
+            <section>
+                <div className="container">
+                    <div className="blog-details-body">
+                        <div className="blog-detail-head">
+                            <div className="cover-img-blog-detail">
+                                <img src={blogimg} alt="" />
+                                <a>Catagory</a>
+                            </div>
+                            <div className="blog-detail-title">
+                                <h2>Culinary Adventures Around the World: Exploring Global Cuisine and Food Tourism</h2>
+                                <div className="blog-author-detail">
+                                    <div className="blog-author">
+                                        <img src={bannerImg} alt="" />
+                                        <h4>Tanvir kabir</h4>
+                                        <p>Apr 25, 2024</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr />
+                            <div className="blog-details">
+                                <p>While blockchain technology gained prominence as the underlying technology behind cryptocurrencies like Bitcoin, its applications extend far beyond the realm of digital currencies. Blockchain is essentially a decentralized, tamper-proof ledger that records transactions across a network of computers.</p>
+
+                                <p>In this blog post, we will explore some of the diverse applications of blockchain technology:</p>
+
+                                <h4>Supply Chain Management:</h4>
+
+                                <p>Blockchain enables transparent and traceable supply chains by recording every transaction from the production of goods to their delivery to consumers. This helps prevent fraud, counterfeiting, and supply chain disruptions while ensuring product authenticity and quality.</p>
+
+                                <h4>Identity Verification:</h4>
+
+                                <p>Blockchain-based identity systems offer a secure and decentralized way to verify individuals identities without relying on a central authority. This has implications for areas such as voting systems, passport issuance, and access to financial services for the unbanked.</p>
+
+                                <h4>Smart Contracts:</h4>
+
+                                <p>Smart contracts are self-executing contracts with the terms of the agreement directly written into code. They automatically enforce and execute the terms of the contract when predefined conditions are met, eliminating the need for intermediaries and reducing the risk of disputes.</p>
+
+                                <p>Despite its potential, blockchain technology still faces challenges such as scalability, interoperability, and regulatory uncertainty. However, as blockchain continues to mature and evolve, its transformative impact on various industries is becoming increasingly evident.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
